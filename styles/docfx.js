8b9cc34b9c50d2cb2b3c5a0620e162494650d2ac
oofdesu <41898282+github-actions[bot]@users.noreply.github.com>
2022-01-17 01:06:34 +0000

    oofdesu published a site update

--- styles/docfx.js
+++ styles/docfx.js
@@ -234,7 +234,7 @@ $(function () {
     // Highlight the searching keywords
     function highlightKeywords() {
       var q = url('?q');
-      if (q !== null) {
+      if (q) {
         var keywords = q.split("%20");
         keywords.forEach(function (keyword) {
           if (keyword !== "") {
